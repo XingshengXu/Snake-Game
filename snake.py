86ed3176ae2a6e49f15ce1c25058c85e22b5d5d8
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-04-19 17:01:38 -0700

    Version 1.0

--- snake.py
+++ snake.py
@@ -8,7 +8,11 @@ WIDTH, HEIGHT = 800, 800
 GRID_WIDTH = 40
 ROW_NUMBER = WIDTH // GRID_WIDTH
 
-# FPS, Direction, and Moving Speed
+# Game Timer
+REACTION_TIME = 100
+DELAY_TIME = 4000
+
+# FPS, Directions, and Moving Speed
 FPS = 5
 MOVING_SPEED = GRID_WIDTH
 UP, DOWN, LEFT, RIGHT = range(4)
@@ -37,9 +41,12 @@ MUSIC = 'assets/sound/snake_bgm.mp3'
 
 # Game Events
 SNAKE_GROW = pg.USEREVENT + 1
+SNAKE_HIT = pg.USEREVENT + 2
 
 
 class Snake(pg.sprite.Sprite):
+    '''This class represents the snake object in the game.'''
+
     def __init__(self, idx=0, ishead=True, position=None, direction=LEFT):
         super().__init__()
         self.dir = direction
@@ -48,7 +55,7 @@ class Snake(pg.sprite.Sprite):
         self.prev_pos = position
         self.prev_dir = direction
 
-        # Load Snake Head Image
+        # Load and Transform Snake Head and Body Image
         if self.ishead:
             self.head_image = {}
             for i in range(len(SNAKE_HEAD)):
@@ -58,7 +65,6 @@ class Snake(pg.sprite.Sprite):
                 self.head_image[i] = head_image
             self.image = self.head_image[LEFT]
         else:
-            # Load Snake Body Image
             body_image = pg.image.load(SNAKE_BODY).convert_alpha()
             self.image = pg.transform.scale(
                 body_image, (GRID_WIDTH, GRID_WIDTH))
@@ -68,6 +74,9 @@ class Snake(pg.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=position)
 
     def move(self):
+        '''Move the snake in the specified direction, updating its position accordingly. 
+        The head's direction can be updated by arrow key inputs, and the body parts follow 
+        the head and preceding body parts.'''
         if self.ishead:
             # Store the previous direction and position of the head
             self.prev_dir = self.dir
@@ -103,7 +112,7 @@ class Snake(pg.sprite.Sprite):
             self.rect.topleft = prev_body.prev_pos
             self.dir = prev_body.prev_dir
 
-            # Update the previous position of the current body part
+            # Update the previous position of the body part
             self.prev_pos = temp_pos
             self.prev_dir = temp_dir
 
@@ -112,6 +121,8 @@ class Snake(pg.sprite.Sprite):
 
 
 class Food(pg.sprite.Sprite):
+    '''It represents the food object in the game.'''
+
     def __init__(self):
         super().__init__()
 
@@ -121,6 +132,8 @@ class Food(pg.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=self.food_spawn())
 
     def food_spawn(self):
+        '''Generate a random position for the food.'''
+
         while True:
             food_x = randint(0, ROW_NUMBER - 1) * GRID_WIDTH
             food_y = randint(0, ROW_NUMBER - 1) * GRID_WIDTH
@@ -130,15 +143,18 @@ class Food(pg.sprite.Sprite):
             if all(body.rect.topleft != food_position for body in snake.sprites()):
                 return food_position
 
-    def update(self):
-        pass
-
 
 class Game:
+    '''It initializes the game window, clock, and the Pygame library. 
+    It loads and plays background music, as well as the bite and hit sound effects. 
+    It also draws the grid, grass background, and score text.'''
+
     def __init__(self):
         pg.init()
         self.clock = pg.time.Clock()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.game_message = ''
+        self.font = pg.font.SysFont('comicsans', 30)  # Message Font
         pg.display.set_caption('Snake Game')
 
         # Load Background Images
@@ -147,15 +163,16 @@ class Game:
             self.grass_background, (GRID_WIDTH, GRID_WIDTH))
 
         # Load Game Music
-        # pg.mixer.music.load(MUSIC)
-        # pg.mixer.music.set_volume(0.5)
-        # pg.mixer.music.play(loops=-1)
+        pg.mixer.music.load(MUSIC)
+        pg.mixer.music.set_volume(0.5)
+        pg.mixer.music.play(loops=-1)
 
         # Load Sound Effect
         self.bite_sound = pg.mixer.Sound(BITE_SOUND)
         self.hit_sound = pg.mixer.Sound(HIT_SOUND)
 
     def draw_grid(self):
+        '''Draw grid on screen.'''
         x, y = 0, 0
         for _ in range(ROW_NUMBER):
             x = x + GRID_WIDTH
@@ -165,11 +182,20 @@ class Game:
             pg.draw.line(self.screen, 'black', (0, y), (WIDTH, y))
 
     def draw_grass(self):
+        '''Draw grass on screen.'''
         for x in range(0, WIDTH, GRID_WIDTH):
             for y in range(0, HEIGHT, GRID_WIDTH):
                 self.screen.blit(self.grass_background, (x, y))
 
+    def draw_score(self, text):
+        '''Draws the score message on the game display.'''
+        draw_text = self.font.render(text, False, 'red')
+        self.screen.blit(draw_text, (WIDTH//2 - draw_text.get_width() //
+                                     2, HEIGHT//2 - draw_text.get_height()//2))
+        pg.display.update()
+
     def snake_grow(self):
+        '''Extend the length of the snake by adding a new body part.'''
         last_body = snake.sprites()[-1]
         idx = last_body.idx
         pos_x, pos_y = last_body.rect.topleft
@@ -183,7 +209,16 @@ class Game:
                   position=newbody_location[last_body.dir], direction=last_body.dir))
 
     def collision(self):
-        # Check Collision with Food
+        '''Handle collisions between the snake and food or 
+        between the snake's head and its body.'''
+        # Collision with Itself
+        head = snake.sprites()[0]
+        body = snake.sprites()[1:]
+        if pg.sprite.spritecollide(head, body, False):
+            self.hit_sound.play()
+            pg.event.post(pg.event.Event(SNAKE_HIT))
+
+        # Collision with Food
         if pg.sprite.spritecollide(food.sprite, snake, False):
             self.bite_sound.play()
             pg.event.post(pg.event.Event(SNAKE_GROW))
@@ -191,16 +226,28 @@ class Game:
             food.add(Food())
 
     def handle_events(self):
+        '''Process Pygame events, including quitting the game, 
+        collisions, and snake growth.'''
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 exit()
+            if event.type == SNAKE_HIT:
+                self.game_message = f'Game Over! Wow, the snake length is {len(snake.sprites())}!'
+                self.draw_score(self.game_message)
+                pg.time.delay(DELAY_TIME)
+                self.game_message = ''
+                snake.empty()
+                food.empty()
+                snake.add(Snake())
+                food.add(Food())
+
             if event.type == SNAKE_GROW:
                 self.snake_grow()
 
     def main_loop(self):
         while True:
-            pg.time.delay(100)  # Delay for User Reaction
+            pg.time.delay(REACTION_TIME)  # Delay for User Reaction
             self.clock.tick(FPS)
 
             # Handle Events
